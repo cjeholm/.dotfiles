32d8d18aaf6f225bc413544ddf23ebf91dccbb46
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-04-03 19:03:32 +0200

    Rewmoved fish greeting. Added nvim pylsp config to end of init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -573,5 +573,19 @@ cmp.setup {
 
 vim.cmd.colorscheme('darcula-solid')
 
+-- pylsp config.
+require'lspconfig'.pylsp.setup{
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W391'},
+          maxLineLength = 120
+        }
+      }
+    }
+  }
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
