6774699555f392056ac30828a7feeb6753077e07
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-04-06 20:50:01 +0200

    stuff

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,5 +4,7 @@ return {
   { 'nvim-lua/plenary.nvim'},
   { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }, opts = {} },
   { "nvim-tree/nvim-web-devicons", opts = {} },
-  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {indent={char="⎸"}} },
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {indent={char="┆"}} },
+  { 'lewis6991/gitsigns.nvim', opts = {} },
+
 }
