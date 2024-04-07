77f33e4ebc4e3288880116be72e71d8d3f549892
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-04-07 21:39:07 +0200

    Removed pkg-installs

--- install.sh
+++ install.sh
@@ -4,38 +4,25 @@
 # by Conny Holm 2024
 
 packages=(
-	"alacritty"
-	"neofetch"
-	"neovim"
-	"qtile"
-	"rofi"
-	"zsh"
-	"qt5ct"
-	"i3lock"
-	"python-iwlib"
-	"ttc-iosevka"
-	"eza"
-	"networkmanager"
-	"stow"
 )
 
 configs=(
 	"alacritty"
+	"fish"
 	"git"
 	"neofetch"
 	"nvim"
 	"qtile"
 	"rofi"
-	"fish"
 )
 
 # INSTALL PACKAGES
-echo -e 'Installing packages:'
-installCommand="sudo pacman -S --needed "
-for str in ${packages[@]}; do
-	installCommand+=$str" "
-done
-echo $installCommand
+# echo -e 'Installing packages:'
+# installCommand="sudo pacman -S --needed "
+# for str in ${packages[@]}; do
+# 	installCommand+=$str" "
+# done
+# echo $installCommand
 # $installCommand
 
 # STOW DOTFILES
@@ -44,9 +31,9 @@ for str in ${configs[@]}; do
 	echo stow $str
 	stow $str
 done
-#
+
 # SET ENVIRONMENT VARIABLES
-echo -e '\nManual steps:'
-echo -e 'Add QT_QPA_PLATFORMTHEME=qt5ct to /etc/environment'
+# echo -e '\nManual steps:'
+# echo -e 'Add QT_QPA_PLATFORMTHEME=qt5ct to /etc/environment'
 
 
