9febd529aa07ae5fa653c1c60012dece6d61bfc9
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-07-07 09:53:54 +0200

    Added telescope buffers dialog

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -17,6 +17,7 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Search recent" })
 
       require("telescope").load_extension("ui-select")
