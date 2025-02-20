f4a54a50fe516d704b903aabdfe4b8fba06cb748
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-02-20 07:30:02 +0100

    Added keybind

--- nvim/.config/nvim/lua/plugins/findbacklinks.lua
+++ nvim/.config/nvim/lua/plugins/findbacklinks.lua
@@ -1,7 +1,8 @@
 return {
-  "cjeholm/findbacklinks",
-  config = function()
-    require("findbacklinks")
-  end,
-  lazy = false,
+	"cjeholm/findbacklinks",
+	config = function()
+		require("findbacklinks")
+	end,
+	lazy = false,
+	vim.keymap.set("n", "gb", "<cmd>FindBacklinks<cr>", { desc = "Find Backlinks" }),
 }
