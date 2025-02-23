41b1d0e27a4812c6522c64586aa73ed58a0c0387
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-02-23 21:43:39 +0100

    keybind for findbacklinks outgoing

--- nvim/.config/nvim/lua/plugins/findbacklinks.lua
+++ nvim/.config/nvim/lua/plugins/findbacklinks.lua
@@ -1,8 +1,11 @@
 return {
 	"cjeholm/findbacklinks",
+  -- dev = true, -- enable dev mode
+  -- dir ="~/Documents/lua/findbacklinks", -- use local path
 	config = function()
 		require("findbacklinks")
 	end,
 	lazy = false,
 	vim.keymap.set("n", "gb", "<cmd>FindBacklinks<cr>", { desc = "Find Backlinks" }),
+	vim.keymap.set("n", "go", "<cmd>FindOutlinks<cr>", { desc = "Find Outgoing links" }),
 }
