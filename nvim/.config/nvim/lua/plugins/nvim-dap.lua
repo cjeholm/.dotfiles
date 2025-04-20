ab0fc9e6e23f6791c7e5626b05c0c68ade7fd4d9
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-04-20 13:31:00 +0200

    nvim-dap

---
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,5 @@
+return {
+	"mfussenegger/nvim-dap",
+	-- version = "^6", -- Recommended
+	lazy = false, -- This plugin is already lazy
+}
--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,6 +1,10 @@
-return
-{
-  "mrcjkb/rustaceanvim",
-  version = "^6", -- Recommended
-  lazy = false,  -- This plugin is already lazy
+return {
+	"mrcjkb/rustaceanvim",
+	version = "^6", -- Recommended
+	lazy = false, -- This plugin is already lazy
+	["rust-analyzer"] = {
+		cargo = {
+			allFeatures = true,
+		},
+	},
 }
