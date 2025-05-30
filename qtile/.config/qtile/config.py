3bc1a2a33c9aa7d2119060ad3b758c6d1a9637dc
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-05-30 18:31:20 +0200

    kitty for laptop

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -51,7 +51,8 @@ def set_date_to_clipboard(foo):
 
 mod = "mod4"
 # terminal = guess_terminal()
-terminal = "alacritty"
+# terminal = "alacritty"
+terminal = "kitty"
 # terminal = "konsole"
 
 keys = [
