b680f4152df903f6008a8860b7a723501939bc77
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-11-07 18:25:19 +0100

    animation without smooth scrolling

--- nvim/.config/nvim/lua/plugins/mini-nvim.lua
+++ nvim/.config/nvim/lua/plugins/mini-nvim.lua
@@ -4,17 +4,21 @@ return {
     version = false,
     config = function()
       require("mini.ai").setup()
-      require("mini.surround").setup()  -- saw"
+      require("mini.surround").setup() -- saw"
       require("mini.git").setup()
       require("mini.diff").setup()
       -- require("mini.completion").setup()
       -- require("mini.snippets").setup()
       require("mini.icons").setup()
-      require("mini.move").setup()  -- alt + hjkl
+      require("mini.move").setup() -- alt + hjkl
       require("mini.pairs").setup()
       require("mini.indentscope").setup()
       require("mini.statusline").setup()
-      -- require("mini.animate").setup()
+
+      local minianim = require("mini.animate")
+      minianim.setup({
+        scroll = { enable = false },
+      })
 
       local miniclue = require("mini.clue")
       miniclue.setup({
